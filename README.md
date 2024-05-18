b61d80ee9e1c91c5a8282945e921b4776d6c4790
San <santiagocerutti1@gmail.com>
2024-05-18 19:21:42 -0300

    dash app update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# reports
-# pip install dash-tools
+# Pasos para hacer una app de dash y hacer el deploy en render
+**Instalar**
+~~~
+pip install dash-tools
+
+dashtools gui
+~~~
+**Estructura de carpetas**
+~~~
+Carpeta del proyecto/
+├── data/
+│   └── datos.csv (o lo que sea)
+├── src/
+│   └── app.py
+├── requirements.txt
+└── render.yaml
+~~~
+
+**Requisitos**
+* Que exitan los archivos:
+  - src/app.py
+  - render.yaml (Se puede generar automáticamente con el Dash Tools, indicando previamente el Render App Name)
+  - requirements.txt (Se puede generar automáticamente con el Dash Tools )
+* El proyecto debe estar Pushed to GitHub Public repository
+* Debe estar la línea: `server = app.server` en el archivo src/app.py (después de `app = Dash(__name__)` )
--- src/app.py
+++ src/app.py
@@ -12,28 +12,71 @@ df = pd.read_csv(os.path.join(os.path.dirname(__file__),'..','data', 'Municipali
 app = Dash(__name__)
 server = app.server
 
+# Lista de variables
 vars = df.columns[5:]
 
+# Lista de etiquetas etiquetas
+variable_options = [
+    {'label': 'Tri Mean', 'value': 'tri1k_mean'},
+    {'label': 'VRM Mean', 'value': 'vrm1k_mean'},
+    {'label': 'ROU Mean', 'value': 'rou1k_mean'},
+    {'label': 'SLP Mean', 'value': 'slp1k_mean'},
+    {'label': 'Tri Sd', 'value': 'tri1k_sd'},
+    {'label': 'VRM Sd', 'value': 'vrm1k_sd'},
+    {'label': 'ROU Sd', 'value': 'rou1k_sd'},
+    {'label': 'SLP Sd', 'value': 'slp1k_sd'},
+]
+
+# Diccionario de etiquetas
+label_map = {option['value']: option['label'] for option in variable_options}
+
 app.layout = html.Div([
-    html.H4('Matrix correlations'),
-    dcc.Dropdown(
-        id="dropdown",
-        options=vars,
-        value=['tri1k_mean', 'vrm1k_mean', 'rou1k_mean','slp1k_mean'],
-        multi=True
-    ),
-    dcc.Graph(id="graph"),
+    html.H4('Matrix Correlations'),
+    html.Div([
+        html.Label('Select Variables:'),
+        dcc.Dropdown(
+            id="dropdown",
+            options=variable_options,
+            value=['tri1k_mean', 'vrm1k_mean', 'rou1k_mean', 'slp1k_mean'],
+            multi=True,
+            style={'width': '80%'}
+        ),
+    ], style={'width': '100%', 'display': 'flex', 'justify-content': 'center', 'margin-bottom': '20px'}),
+    
+    html.Div([
+        html.H5('Scatter Matrix Plot'),
+        dcc.Graph(id="scatter_matrix", style={'height': '70vh'})
+    ], style={'width': '100%', 'display': 'flex', 'justify-content': 'center', 'flexDirection': 'column', 'align-items': 'center'}),
+
+    html.Div([
+        html.H5('Strip Plot'),
+        dcc.Graph(id="strip_plot", style={'height': '70vh'})
+    ], style={'width': '100%', 'display': 'flex', 'justify-content': 'center', 'flexDirection': 'column', 'align-items': 'center'})
 ])
 
 
 @app.callback(
-    Output("graph", "figure"), 
-    Input("dropdown", "value"))
+    [Output("scatter_matrix", "figure"), Output("strip_plot", "figure")], 
+    [Input("dropdown", "value")]
+)
 def update_bar_chart(dims):
     fig = px.scatter_matrix(
-        df, dimensions=dims)
+        df, 
+        dimensions=dims,
+        labels={dim: label_map[dim] for dim in dims},
+        height=500,
+        width=800
+    )
     fig.update_traces(diagonal_visible=False)
-    return fig
+    
+    fig2 = px.strip(df,
+                    x=dims,
+                    hover_name="NOMGEO",
+                    color="CVE_ENT",
+                    height=500,
+                    width=800
+    )
+    return fig,fig2
 
 if __name__ == "__main__":
     app.run_server(debug=True,port=8072)
