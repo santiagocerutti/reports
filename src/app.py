b3ba1956dfdb2d39461d23e971de4482a4982fbb
San <santiagocerutti1@gmail.com>
2024-05-18 17:40:45 -0300

    dash app update

--- src/app.py
+++ src/app.py
@@ -2,19 +2,23 @@ from dbfread import DBF
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+import os
 
-dbf = DBF('D:/Documentos/Apps Python/reports/data/Municipalities_with_topo.dbf', encoding='utf-8')
+# Read data
+dbf = DBF(os.path.abspath('data/Municipalities_with_topo.dbf'), encoding='utf-8')
 df = pd.DataFrame(iter(dbf))
 
 
 app = Dash(__name__)
-server = app.server in app.py
+server = app.server
+
+vars = df.columns[5:]
 
 app.layout = html.Div([
     html.H4('Matrix correlations'),
     dcc.Dropdown(
         id="dropdown",
-        options=['tri1k_mean', 'vrm1k_mean', 'rou1k_mean','slp1k_mean'],
+        options=vars,
         value=['tri1k_mean', 'vrm1k_mean', 'rou1k_mean','slp1k_mean'],
         multi=True
     ),
@@ -28,7 +32,8 @@ app.layout = html.Div([
 def update_bar_chart(dims):
     fig = px.scatter_matrix(
         df, dimensions=dims)
+    fig.update_traces(diagonal_visible=False)
     return fig
 
 #http://127.0.0.1:8071/
-app.run_server(debug=True,port=8071) 
+app.run_server(debug=True,port=8072) 
